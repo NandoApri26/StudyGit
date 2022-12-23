310bacb9678177dddb009594294c6387ceb7a922
YOUR NAME <YOUR EMAIL@SAMLECOM>
2022-12-23 22:04:09 +0700

    js

--- dump/suya.js
+++ dump/suya.js
@@ -4,3 +4,16 @@ function index(params) {
 
 console.log(index("isman"));
 asdsad;
+
+(function(factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof exports !== 'undefined') {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(jQuery);
+    }
+
+  }
+)
